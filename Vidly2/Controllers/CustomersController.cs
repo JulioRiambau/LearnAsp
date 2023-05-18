0d5b535414c767142f0c49a897908824a97c2fb0
Julio Riambau <julio.riambau@skyline.be>
2023-05-18 11:27:57 -0400

    exercise 1

---
+++ Vidly2/Controllers/CustomersController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using Vidly2.Models;
+using Vidly2.ViewModels;
+
+namespace Vidly2.Controllers
+{
+    public class CustomersController : Controller
+    {
+        public List<Customer> customers = new List<Customer>()
+        {
+            new Customer { Id = 1, Name = "julio"},
+            new Customer { Id = 2, Name = "antonio"},
+        };
+
+        //public List<Customer> customers = new List<Customer>();
+
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var viewCustomers = new IndexCustomersViewModel() { Customers = customers };
+
+            return View(viewCustomers);
+        }
+
+        [Route("customers/details/{id}")]
+        public ActionResult Details(int id)
+        {
+            var customer = customers.FirstOrDefault(x => x.Id == id);
+
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(customer);
+        }
+    }
+}
--- Vidly2/Controllers/MoviesController.cs
+++ Vidly2/Controllers/MoviesController.cs
@@ -11,6 +11,13 @@ namespace Vidly2.Controllers
 {
     public class MoviesController : Controller
     {
+
+        public List<Movie> movies = new List<Movie>()
+        {
+            new Movie() { Id = 1, Name = "Shrek" },
+            new Movie() { Id = 2, Name = "Wall-e" }
+        };
+
         // GET: Movie
         public ActionResult Random()
         {
@@ -18,11 +25,7 @@ namespace Vidly2.Controllers
             var customers = new List<Customer>()
             {
                 new Customer { Id = 1, Name = "julio"},
-                new Customer { Id=2, Name = "antonio1"},
-                new Customer { Id=3, Name = "antonio2"},
-                new Customer { Id=4, Name = "antonio3"},
-                new Customer { Id=5, Name = "antonio4"},
-                new Customer { Id=6, Name = "antonio5"},
+                new Customer { Id=2, Name = "antonio"},
             };
 
             var viewModel = new RandomMovieViewModel()
@@ -44,25 +47,27 @@ namespace Vidly2.Controllers
             return Content("id=" + id);
         }
 
-        public ActionResult Index(int? pageIndex, string sortBy)
+        public ActionResult Index()
         {
-            if (!pageIndex.HasValue)
-            {
-                pageIndex = 1;
-            }
-
-            if (sortBy.IsNullOrWhiteSpace())
-            {
-                sortBy = "Name";
-            }
-
-            return Content($"pageindex={pageIndex} sortBy={sortBy}");
+            return View(new IndexMoviewsViewModel(){Movies = movies});
         }
 
         [Route("movies/released/{year}/{month:range(1,12)}")]
         public ActionResult ByReleaseDate(int year, int month)
         {
             return Content($"{year}/{month}");
+        }
+
+        public ActionResult Details(int id)
+        {
+            var customer = movies.FirstOrDefault(x => x.Id == id);
+
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(customer);
         }
     }
 }
