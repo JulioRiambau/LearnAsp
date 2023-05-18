027c6656ca79f7c5effa1744cea074236b03f837
Julio Riambau <julio.riambau@skyline.be>
2023-05-18 10:36:30 -0400

    initial version

---
+++ Vidly2/Controllers/MoviesController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.Ajax.Utilities;
+using Vidly2.Models;
+using Vidly2.ViewModels;
+
+namespace Vidly2.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movie
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek" };
+            var customers = new List<Customer>()
+            {
+                new Customer { Id = 1, Name = "julio"},
+                new Customer { Id=2, Name = "antonio1"},
+                new Customer { Id=3, Name = "antonio2"},
+                new Customer { Id=4, Name = "antonio3"},
+                new Customer { Id=5, Name = "antonio4"},
+                new Customer { Id=6, Name = "antonio5"},
+            };
+
+            var viewModel = new RandomMovieViewModel()
+            {
+                Movie = movie,
+                Customers = customers,
+                //Customers = new List<Customer>(),
+            };
+
+            return View(viewModel);
+            //return Content("Hello world!");
+            //return HttpNotFound();
+            //return new EmptyResult();
+            //return RedirectToAction("Index", "Home", new {page=1, sortBy="name"});
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+
+            if (sortBy.IsNullOrWhiteSpace())
+            {
+                sortBy = "Name";
+            }
+
+            return Content($"pageindex={pageIndex} sortBy={sortBy}");
+        }
+
+        [Route("movies/released/{year}/{month:range(1,12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content($"{year}/{month}");
+        }
+    }
+}
