9c71287be5db7dc60786f9b8667e6a57b7ceecdf
Julio Riambau <julio.riambau@skyline.be>
2023-05-18 14:09:45 -0400

    adds entity data

--- Vidly2/Controllers/CustomersController.cs
+++ Vidly2/Controllers/CustomersController.cs
@@ -3,26 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using Vidly2.Data;
 using Vidly2.Models;
 using Vidly2.ViewModels;
+using System.Data.Entity;
 
 namespace Vidly2.Controllers
 {
     public class CustomersController : Controller
     {
-        public List<Customer> customers = new List<Customer>()
-        {
-            new Customer { Id = 1, Name = "julio"},
-            new Customer { Id = 2, Name = "antonio"},
-        };
+        //public List<Customer> customers = new List<Customer>()
+        //{
+        //    new Customer { Id = 1, Name = "julio"},
+        //    new Customer { Id = 2, Name = "antonio"},
+        //};
 
         //public List<Customer> customers = new List<Customer>();
+
+        private Vidly2Context _context;
+
+        public CustomersController()
+        {
+            _context = new Vidly2Context();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+            base.Dispose(disposing);
+        }
 
         // GET: Customers
         public ActionResult Index()
         {
-            var viewCustomers = new IndexCustomersViewModel() { Customers = customers };
+            var viewCustomers = _context.Customers.Include(c => c.MembershipType).ToList();
 
             return View(viewCustomers);
         }
@@ -30,7 +44,7 @@ namespace Vidly2.Controllers
         [Route("customers/details/{id}")]
         public ActionResult Details(int id)
         {
-            var customer = customers.FirstOrDefault(x => x.Id == id);
+            var customer = _context.Customers.FirstOrDefault(x => x.Id == id);
 
             if (customer == null)
             {
---
+++ Vidly2/Data/Vidly2Context.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using Vidly2.Models;
+
+namespace Vidly2.Data
+{
+    public class Vidly2Context : DbContext
+    {
+        public Vidly2Context() : base()
+        {
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<MembershipType> MembershipTypes { get; set; }
+    }
+}
